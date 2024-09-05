c51fd28143944b625714d5aba1e72bf81c881a9e
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-09-06 00:25:04 +1000

    add marquee to show the status

--- src/components/menu_components/BlogHeader.js
+++ src/components/menu_components/BlogHeader.js
@@ -1,6 +1,7 @@
 // src/components/Navbar.js
 import React from "react";
 import MenuButton from "./MenuButton";
+import Marquee from "react-fast-marquee";
 
 export default function BlogHeader() {
   return (
@@ -9,6 +10,12 @@ export default function BlogHeader() {
         <a className="font-bold text-lg text-black">
           <a href="#about">Wilson Liu</a>
         </a>
+        <div className="w-1/2 hover:w-2/3 flex h-16 items-center">
+          <Marquee className="bg-gray-100 h-8 hover:h-12 rounded-full ">
+            Avaliable for new positiions
+          </Marquee>
+        </div>
+
         <div className="flex items-center space-x-4">
           <MenuButton />
         </div>
