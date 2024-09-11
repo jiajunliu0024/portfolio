26b9333770cbd3548d5a4c0d478f990d86c6bcf4
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-09-11 14:25:57 +1000

    add the router to switch the pages

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import Skills from "./components/Skills";
 import Projects from "./components/project_components/Projects";
 import BlogHeader from "./components/menu_components/BlogHeader";
 import Experience from "./components/experience_components/Experience";
+import ComingSoon from "./components/ComingSoon";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <main>
-      <BlogHeader />
-      <About />
-      <Projects />
-      <Experience />
-      <Skills />
-      <Contact />
-    </main>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main>
+              <BlogHeader />
+              <About />
+              <Projects />
+              <Experience />
+              <Skills />
+              <Contact />
+            </main>
+          }
+        />
+        <Route path="/coming-soon" element={<ComingSoon />} />
+      </Routes>
+    </Router>
   );
 }
 
