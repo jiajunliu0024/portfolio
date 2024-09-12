014946f6f38208217b92b44f19aa08ec084e23f7
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-09-12 18:50:30 +1000

    update Testimonials

--- src/components/project_components/Projects.js
+++ src/components/project_components/Projects.js
@@ -10,7 +10,7 @@ export default function Projects() {
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto text-black inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-black">
+          <h1 className="sm:text-4xl text-3xl font-medium body-font mb-4 text-black">
             Apps I've Built
           </h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base"></p>
