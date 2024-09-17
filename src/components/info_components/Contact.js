a17cc664d547172694925f633372a37ed88133db
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-09-17 11:56:52 +1000

    1. correct the contact form navigation button 2. add the pop up window to validate the fields has been filled in

--- src/components/info_components/Contact.js
+++ src/components/info_components/Contact.js
@@ -1,14 +1,29 @@
 // src/components/Contact.js
-import { Button } from "antd";
 import React, { useEffect } from "react";
 import BlogHeader from "../menu_components/BlogHeader";
+import Button from "@mui/material/Button";
 import "../../App.css";
+
+function DisplayValidationError({ hiddenStatus, message }) {
+  return (
+    hiddenStatus && (
+      <span
+        key={message}
+        hidden={hiddenStatus}
+        className="text-left w-full ml-2 text-xs my-2 text-red-500"
+      >
+        {message}
+      </span>
+    )
+  );
+}
 
 export default function Contact() {
   const [hasEmail, setHasEmail] = React.useState(true);
   const [hasMessage, setHasMessage] = React.useState(true);
   const [hasName, setHasName] = React.useState(true);
   const [validEmail, setValidEmail] = React.useState(true);
+  const [sendMsgClick, secSendMsgClick] = React.useState(false);
   const [name, setName] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [message, setMessage] = React.useState("");
@@ -20,51 +35,27 @@ export default function Contact() {
   };
 
   useEffect(() => {
-    if (name.length > 0) {
-      setHasName(true);
-    } else {
-      setHasName(false);
-    }
+    console.log("hasName", hasName);
+    name.length > 0 ? setHasName(true) : setHasName(false);
   }, [name]);
 
   useEffect(() => {
-    if (email.length > 0) {
-      setHasEmail(true);
-    } else {
-      setHasEmail(false);
-    }
+    console.log("hasEmail", hasEmail);
+    email.length > 0 ? setHasEmail(true) : setHasEmail(false);
   }, [email]);
 
   useEffect(() => {
-    if (email.length > 0) {
-      const re = /\S+@\S+\.\S+/;
-      setValidEmail(re.test(email));
-    } else {
-      setValidEmail(true);
-    }
-    if (message.length > 0) {
-      setHasMessage(true);
-    } else {
-      setHasMessage(false);
-    }
+    message.length > 0 ? setHasMessage(true) : setHasMessage(false);
+    console.log("hasMessage", hasMessage);
   }, [message]);
 
-  const sendEmail = () => {
-    if (hasEmail && hasMessage && hasName && validEmail) {
-      alert("Email sent successfully");
+  const sendMsg = () => {
+    if (hasEmail && hasMessage && hasName) {
+      alert("Message sent successfully");
+    } else {
+      alert("Please fill in all required fields");
     }
   };
-
-  function displayValidationError(hiddenStatus, message) {
-    return (
-      <span
-        hidden={hiddenStatus}
-        className="text-left w-full ml-2 text-xs my-2 text-red-500"
-      >
-        {message}
-      </span>
-    );
-  }
 
   return (
     <section id="contact" className="">
@@ -73,44 +64,40 @@ export default function Contact() {
         <span className="text-2xl font-bold">Contact</span>
         <form className="flex flex-col items-center mt-10 w-96 p-5 md:p-0">
           <input
-            onChange={(e) => setName(e.target.value)}
+            onInput={(e) => setName(e.target.value)}
             className="p-4 w-full m-2 bg-gray-100 rounded-md border-0 outline-none"
             placeholder="Name"
             name="name"
           ></input>
-          <displayValidationError
+          <DisplayValidationError
             hidden={hasName}
             message={errorMessage.name}
           />
           <input
-            onChange={(e) => setEmail(e.target.value)}
+            onInput={(e) => setEmail(e.target.value)}
             className="p-4 w-full m-2 bg-gray-100 rounded-md border-0 outline-none"
             placeholder="Email"
             name="email"
           ></input>
-          <displayValidationError
+          <DisplayValidationError
             hidden={hasEmail}
             message={errorMessage.email}
           />
-          <displayValidationError
-            hidden={validEmail}
-            message={errorMessage.validEmail}
-          />
           <textarea
-            onChange={(e) => setMessage(e.target.value)}
+            onInput={(e) => setMessage(e.target.value)}
             className="p-4 m-2 w-full max-h-full border-0 bg-gray-100 rounded-md outline-none"
             rows="4"
-            placeholder="What's on your mind"
+            placeholder="Let's have a chat"
             name="message"
           ></textarea>
-          <displayValidationError
+          <DisplayValidationError
             hidden={hasMessage}
             message={errorMessage.message}
           />
         </form>
-        <Button onClick={sendEmail} className="button-submit">
+        <button onClick={sendMsg} className="button-submit">
           Submit
-        </Button>
+        </button>
       </div>
     </section>
   );
